1ce93dfd04265a3fd40723e4175363a33ac173a4
Menka shah <105760147+menkashah445@users.noreply.github.com>
2024-03-14 01:16:20 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,10 +1,7 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
+
 st.write('Hello, *World!* :sunglasses:')
 st.balloons()
-arr = np.random.normal(1, 1, size=100)
-fig, ax = plt.subplots()
-ax.hist(arr, bins=20)
 
-st.pyplot(fig)
+
+
